ab928ed9d3f54ffa2925cec93eb87f1d15aa1d14
Padraic Renaghan <prenagha@renaghan.com>
2018-07-11 22:05:15 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# quick-reply.mmbundle
+MailMate Bundle to implement quick message replies. Similar to the feature used in many mail clients today. I like the [Spark](https://sparkmailapp.com) implementation and used it as a guide for this bundle.
+
+Make sure you have `python3` installed, [http://docs.python-guide.org/en/latest/starting/install3/osx/](http://docs.python-guide.org/en/latest/starting/install3/osx/)
+
---
+++ Support/bin/quick-reply.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+#
+# Quick Reply Messages
+#
+import sys
+import datetime
+import os
+
+THANKS = "\n\nThanks,\nP\n"
+reply = sys.argv[1]
+
+if reply == "like":
+  body = "👍🏻"
+  close = THANKS
+elif reply == "thanks":
+  body = "✅ Thanks"
+  close = ""
+elif reply == "agreed":
+  body = "🤝 Agreed"
+  close = THANKS
+else:
+  raise ValueError("Invalid reply type " + reply)
+
+out = '''
+{ actions = (
+	{
+		type = "replyMessage";
+		body = "%s %s"; 
+    resultActions = ( { type = "sendMessage"; } );	
+  },
+  {
+    type = "notify";
+    formatString = "Replied %s";
+  },
+  {
+    type = "moveMessage";
+    mailbox = "archive";
+  },
+  {
+    type    = "changeFlags";
+    enable  = ( "\\Seen", "\\$NotJunk" );
+    disable = ( "\\$Junk", "\\Junk" );
+  },
+); }
+''' % (body, close, body)
+print (out)
+
